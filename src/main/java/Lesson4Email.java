ebe36dad07e26552060521bdc3721a00284a2f20
Boi4ik <Boi4iksii@gmail.com>
2026-04-23 15:06:49 +0300

    Homework 4:email validation

--- src/main/java/Lesson4Email.java
+++ src/main/java/Lesson4Email.java
@@ -2,31 +2,41 @@ public class Lesson4Email {
 
         public static void main(String[] args) {
 
-        String email =
-                "my.home.mail@gmail.com";
-        boolean isGmail =
-                email.endsWith("gmail.com");
-        int countAt = 0;
-        for(int i = 0; i<email.length(); i++) {
+                String email =
+                        "my.home.mail@gmail.com";
+                boolean isGmail =
+                        email.endsWith("gmail.com");
+                int countDogs = 0;
+                for (int i = 0; i < email.length(); i++) {
 
-            if (email.charAt(i) == '@') {
-                    countAt++;
-            }
+                        if (email.charAt(i) == '@') {
+                                countDogs++;
 
+                        }
+                }
+
+                boolean isOneDog = countDogs == 1;
+                int lastDot =
+                        email.lastIndexOf('.');
+                String part1 = email.substring(0, lastDot).replace('.', '_');
+                String part2 = email.substring(lastDot);
+                String result = part1 + part2;
+
+                if (isGmail && isOneDog){
+                        System.out.println(result);
+                        System.out.println("Is Gmail: " + isGmail);
+                        System.out.println("Is one @: " + isOneDog);
+                        System.out.println("Valid email");
+                } else {
+                        System.out.println(result);
+                        System.out.println("Is Gmail: " + isGmail);
+                        System.out.println("Is one @: " + isOneDog);
+                        System.out.println("Invalid email");
+                }
+
+                }
         }
 
-        boolean oneAt = countAt == 1;
-        int lastDot =
-                email.lastIndexOf('.');
-        String part1 = email.substring(0, lastDot).replace('.', '_');
-        String part2 = email.substring(lastDot);
-        String result = part1 + part2;
 
-        System.out.println(isGmail);
-        System.out.println(oneAt);
-        System.out.println(result);
-    }
-
-}
 
 
