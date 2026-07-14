d4251e65400a4780700a65380b8c370c701046d6
Natalia Kalinichenko <kalinichenkonata62@gmail.com>
2026-07-14 15:45:32 +0300

    Fix Selenium Grid connection for Jenkins

--- src/test/java/org/prog/session13/CucumberRunner.java
+++ src/test/java/org/prog/session13/CucumberRunner.java
@@ -34,7 +34,7 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
     @BeforeSuite
     public void beforeSuite() throws ClassNotFoundException, SQLException, MalformedURLException {
         WebSteps.driver = new RemoteWebDriver(
-                new URL("http://localhost:4444"), remoteChrome());
+                new URL("http://selenium-hub:4444"), remoteChrome());
         Class.forName("com.mysql.cj.jdbc.Driver");
         DBSteps.connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/db", "root", "password");
