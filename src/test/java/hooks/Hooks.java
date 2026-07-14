d30625d84470992d8a9cfc2c77ddd6de01058662
Natalia Kalinichenko <kalinichenkonata62@gmail.com>
2026-07-14 16:41:44 +0300

    Refactor database connection

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,21 +1,19 @@
 package hooks;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.testng.annotations.AfterSuite;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.prog.session14.DBConnectionFactory;
+
+
 
 public class Hooks {
     public static Connection connection;
 
     @Before
     public void before() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
-                "root", "password");
+        connection = DBConnectionFactory.getConnection();
     }
+
 
     @After
     public void after() throws Exception {
